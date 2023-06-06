71595dc863b769aa6962423e21345c75474b11c8
mpGunn <mpg2000@hw.ac.uk>
2023-06-06 15:39:30 +0100

    Adding auto completion feature
    Very good, very nice. Much easier to use.

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Game-Server-Bot
  Python Discord bot capable of running scripts remotely on a Ubuntu host.
+
+Visual of the auto complete working. This was the latest change.
+![img.png](Images/auto_complete_working.png)
--- script_runner.py
+++ script_runner.py
@@ -1,22 +1,70 @@
 import os
 import discord
-from discord.ext import commands
+from discord import app_commands
 import paramiko
 import boto3
+from typing import Optional
 
 TOKEN = os.environ["DISCORD_BOT_TOKEN"]
 SSH_USER = os.environ["SSH_USER"]
 SSH_SERVER_IP = os.environ["SSH_SERVER_IP"]
 SSH_PASSWORD = os.environ["SSH_PASSWORD"]
 
-
 # AWS Configuration
 REGION = os.environ["AWS_REGION"]
 INSTANCE_ID = os.environ["EC2_INSTANCE_ID"]
+MY_GUILD_OS = os.environ["MY_GUILD"]
 
-intents = discord.Intents.all()
-intents.members = True
-bot = commands.Bot(command_prefix="!", intents=intents)
+MY_GUILD = discord.Object(id=MY_GUILD_OS)
+
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+client.tree = app_commands.CommandTree(client)
+
+
+@client.tree.command()
+@app_commands.describe(script_name='The script you want to run', user='The user under whom the script will be run')
+async def run(interaction: discord.Interaction, script_name: str, user: Optional[str] = None):
+    """Runs a script."""
+    try:
+        print(f"Received command: /run {script_name} {user}")
+        output, cwd = run_script(script_name, user)
+        print("Sending output to Discord:")
+        print(f"```\n{output}\n```")
+        await interaction.response.send_message(f"Executing script in directory: `{cwd}`\n```\n{output}\n```")
+    except Exception as e:
+        print(f"Sending error to Discord:")
+        print(f"```\nError: {str(e)}\n```")
+        await interaction.response.send_message(f"```\nError: {str(e)}\n```")
+
+
+@client.tree.command()
+async def ec2_start(interaction: discord.Interaction):
+    """Starts the EC2 instance."""
+    try:
+        ec2 = boto3.client("ec2", region_name=REGION)
+        response = ec2.start_instances(InstanceIds=[INSTANCE_ID])
+        await interaction.response.send_message(f"Starting EC2 instance `{INSTANCE_ID}`: {response}")
+    except Exception as e:
+        await interaction.response.send_message(f"Error starting EC2 instance: {str(e)}")
+
+
+@client.tree.command()
+async def ec2_stop(ctx):
+    try:
+        ec2 = boto3.client("ec2", region_name=REGION)
+        response = ec2.stop_instances(InstanceIds=[INSTANCE_ID])
+        await ctx.send(f"Stopping EC2 instance `{INSTANCE_ID}`: {response}")
+    except Exception as e:
+        await ctx.send(f"Error stopping EC2 instance: {str(e)}")
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} (ID: {client.user.id}) has connected to Discord!')
+    client.tree.copy_global_to(guild=MY_GUILD)
+    await client.tree.sync(guild=MY_GUILD)
+
 
 def run_script(script_name, user=None):
     try:
@@ -50,40 +98,5 @@ def run_script(script_name, user=None):
         print(f"Error: {str(e)}")
         raise Exception(f"Error: {str(e)}")
 
-@bot.event
-async def on_ready():
-    print(f"{bot.user} has connected to Discord!")
 
-@bot.command(name="run")
-async def run_script_command(ctx, script_name, user=None):
-    try:
-        print(f"Received command: !run {script_name} {user}")
-        output, cwd = run_script(script_name, user)
-        print("Sending output to Discord:")
-        print(f"```\n{output}\n```")
-        await ctx.send(f"Executing script in directory: `{cwd}`\n```\n{output}\n```")
-    except Exception as e:
-        print(f"Sending error to Discord:")
-        print(f"```\nError: {str(e)}\n```")
-        await ctx.send(f"```\nError: {str(e)}\n```")
-        
-        
-@bot.command(name="ec2Start")
-async def ec2_start(ctx):
-    try:
-        ec2 = boto3.client("ec2", region_name=REGION)
-        response = ec2.start_instances(InstanceIds=[INSTANCE_ID])
-        await ctx.send(f"Starting EC2 instance `{INSTANCE_ID}`: {response}")
-    except Exception as e:
-        await ctx.send(f"Error starting EC2 instance: {str(e)}")
-
-@bot.command(name="ec2Stop")
-async def ec2_stop(ctx):
-    try:
-        ec2 = boto3.client("ec2", region_name=REGION)
-        response = ec2.stop_instances(InstanceIds=[INSTANCE_ID])
-        await ctx.send(f"Stopping EC2 instance `{INSTANCE_ID}`: {response}")
-    except Exception as e:
-        await ctx.send(f"Error stopping EC2 instance: {str(e)}")
-
-bot.run(TOKEN)
+client.run(TOKEN)
